c6b588c991fb39cbbac79710e575cbd7a7f4da90
ga2gi <sgatugi45@gmail.com>
2026-03-30 17:36:29 +0300

    CHORE: Hardcode keys and force new commit ID

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -5,3 +5,5 @@ const supabaseUrl = 'https://oltwhwgbufarjvqsyzjq.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9sdHdod2didWZhcmp2cXN5empxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE0OTU0NDYsImV4cCI6MjA4NzA3MTQ0Nn0.PpeptTunUNQi-2U_88V-5KtiU6inZ7f_ACRTPboivxE'; 
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+// Force Redeploy
